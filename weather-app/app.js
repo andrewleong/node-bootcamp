befc2d1fc938bbf804c4e56950652b54a834dc7c
andrewleong <andrewleong1993@gmail.com>
2019-10-12 21:43:57 +0800

    Install node fetch

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,1 +1,9 @@
 // Weather app
+const fetch = require("node-fetch");
+
+const url = 'http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22';
+
+fetch(url)
+.then(data => data.json())
+.then(response => console.log("res", response.weather))
+.catch(err => console.error("Error Happened", err))
