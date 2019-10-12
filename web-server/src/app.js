a4f6b68002adde8c53397e11f210d14312d53f3b
andrewleong <andrewleong1993@gmail.com>
2019-10-12 22:46:24 +0800

    Serve public folder

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,25 @@
+// Core Modules
+const path = require('path')
+
+// NPM Modules
 const express = require('express')
 
+// Run express
 const app = express()
 
+// Public Directory Path
+const publicPath = path.join(__dirname, '../public') 
+
+// Customize server to serve static path which is public folder
+app.use(express.static(publicPath))
+
 // Routes
-app.get('', (req, res) => {
-    res.send("Hello Andrew")
-})
-
 app.get('/help', (req, res) => {
-    res.send("Help")
+    res.json({
+        name: 'Andrew',
+        age: 27
+    })
 })
-
 
 app.listen('8000', () => {
     console.log("Listening to port 8000")
